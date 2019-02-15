b600d477e526dcb82692676ef95d5154b731bc4b
steve <chanweijian@126.com>
2019-02-15 20:05:09 +0800

    add app: xmind

---
+++ run-xmind.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# We also add files, xmind.desktop and log-xmind.jpg
+#
+
+# stephen@stephen-desktop:/usr/share/applications$ cat xmind.desktop 
+# [Desktop Entry]
+# Name=XMind
+# Exec=/data/stephen_tools/tools/run-xmind.sh
+# Icon=/usr/share/icons/gnome/128x128/apps/logo-xmind.jpg
+# Type=Application
+# Categories=GTK;GNOME;Office;
+
+# stephen@stephen-desktop:/usr/share/icons/gnome/128x128/apps$ ls logo-xmind.jpg
+# logo-xmind.jpg
+
+cd /home/stephen/Downloads/xmind-8/XMind_amd64
+/home/stephen/Downloads/xmind-8/XMind_amd64/XMind
