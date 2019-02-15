639ea62ec47558f4bec92189032a6f17ba3c0122
steve <chanweijian@126.com>
2019-02-15 20:05:30 +0800

    add app: saleae logic

---
+++ run-saleae-logic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# We also add files, saleae_logic.desktop and logo_saleae_logic.png
+#
+
+# stephen@stephen-desktop:/usr/share/applications$ cat xmind.desktop 
+# [Desktop Entry]
+# Name=Saleae_logic
+# Exec=/data/stephen_tools/tools/run-saleae-logic.sh
+# Icon=/usr/share/icons/gnome/48x48/apps/logo-saleae-logic.png
+# Type=Application
+# Categories=GTK;GNOME;Office;
+
+# stephen@stephen-desktop:/usr/share/icons/gnome/48x48/apps$ ls logo-saleae-logic.png
+# logo-saleae-logic.png
+
+cd /home/stephen/Downloads/saleae_logic/Logic_1.2.18_64-bit
+/home/stephen/Downloads/saleae_logic/Logic_1.2.18_64-bit/Logic
+
