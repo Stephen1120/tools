15abf759269331d618924003e607997e88dc3653
steve <chanweijian@126.com>
2019-02-28 15:58:15 +0800

    adding qq and deleting saleae

---
+++ run-qq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Download: https://github.com/askme765cs/Wine-QQ-TIM
+# We also add files, qq.desktop and qq.png
+#
+
+# stephen@stephen-desktop:/usr/share/applications$ cat app-qq.desktop 
+# [Desktop Entry]
+# Name=QQ
+# Exec=/data/stephen_tools/tools/run-qq.sh
+# Icon=/usr/share/icons/gnome/128x128/apps/logo-qq.png
+# Type=Application
+# Categories=GTK;GNOME;Office;
+
+# stephen@stephen-desktop:/usr/share/icons/gnome/128x128/apps$ ls logo-qq.png
+# logo-qq.png
+
+cd /home/stephen/Downloads/qq
+/home/stephen/Downloads/qq/QQ-20171129-x86_64.AppImage
